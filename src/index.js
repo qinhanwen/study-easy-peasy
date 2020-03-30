97f18a52f3681456a9bcf6065947ea839c3bb005
qinhanwen <285074175@qq.com>
2020-03-30 22:39:05 +0800

    feat: connect store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { StoreProvider } from 'easy-peasy';
+import store from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreProvider store={store}>
     <App />
-  </React.StrictMode>,
+  </StoreProvider>,
   document.getElementById('root')
 );
 
